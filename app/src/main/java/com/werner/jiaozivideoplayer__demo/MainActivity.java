5dcce85048a0ad06ac556ee013035181e692f8b2
Werner <358459199@qq.com>
2019-11-12 11:15:31 +0800

    android x

--- app/src/main/java/com/werner/jiaozivideoplayer__demo/MainActivity.java
+++ app/src/main/java/com/werner/jiaozivideoplayer__demo/MainActivity.java
@@ -1,11 +1,12 @@
 package com.werner.jiaozivideoplayer__demo;
 
 import android.content.res.Configuration;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import cn.jzvd.Jzvd;
 import cn.jzvd.JzvdStd;
